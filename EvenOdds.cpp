619031644a467a9c51ddf8d8f08f645ad115a164
samin710 <98809557+samin710@users.noreply.github.com>
2024-03-20 15:43:56 +0600

    first

--- EvenOdds.cpp
+++ EvenOdds.cpp
@@ -2,56 +2,17 @@
 using namespace std;
 int main()
 {
-    int val[2], x, y = 0;
+    int input[3], x;
 
     for (int i = 0; i < 2; i++)
     {
         cin >> x;
-        val[i] = x;
+        input[i] = x;
     }
-
-    int b, c;
-
-    if (val[0] % 2 == 0)
+    int n = input[0];
+    int k = input[1];
+    for (int i = 0; i < 2; i++)
     {
-        b = val[0] / 2;
+        cout << input[i] << endl;
     }
-    if (val[0] % 2 != 0)
-    {
-        c = (val[0] / 2) + 1;
-    }
-
-    int ma[val[0]];
-
-    for (int i = 1; i <= val[0]; i++)
-    {
-        if (val[0] % 2 == 0)
-        {
-            if (i % 2 != 0)
-            {
-                ma[y] = i;
-                y++;
-            }
-            else
-            {
-                ma[b] = i;
-                b++;
-            }
-        }
-        else
-        {
-            if (i % 2 != 0)
-            {
-                ma[y] = i;
-                y++;
-            }
-            else
-            {
-                ma[c] = i;
-                c++;
-            }
-        }
-    }
-
-    cout << ma[val[1] - 1];
 }
