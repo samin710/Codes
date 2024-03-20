e14f77d458de20a0b174e3dbc14ee141b56f9fde
samin710 <98809557+samin710@users.noreply.github.com>
2024-03-20 16:19:19 +0600

    third

--- EvenOdds.cpp
+++ EvenOdds.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-    int input[3], x, ct = 0;
+    int input[3], x, ct = 0, mid, y = 0;
 
     for (int i = 0; i < 2; i++)
     {
@@ -12,22 +12,28 @@ int main()
     int n = input[0];
     int s = input[1] - 1;
     int final[n];
+    if (n % 2 != 0)
+    {
+        mid = (n / 2) + 1;
+    }
+    else
+    {
+        mid = n / 2;
+    }
 
     for (int i = 1; i <= n; i++)
     {
-        if (i % 2 != 0)
+        if ((i % 2 != 0) && ct <= mid)
         {
             final[ct] = i;
             ct++;
         }
-    }
-    for (int i = 1; i <= n; i++)
-    {
-        if (i % 2 == 0)
+        else
         {
-            final[ct] = i;
-            ct++;
+            final[mid] = i;
+            mid++;
         }
     }
+
     cout << final[s];
 }
